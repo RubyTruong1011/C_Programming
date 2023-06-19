13bf2484fa68856a645def9978500ec30297c7fc
Ruby Truong <96930776+RubyTruong1011@users.noreply.github.com>
2023-06-19 00:32:51 -0400

    check Size

---
+++ Questions/checkSize.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    short a;
+    long b;
+    long long c;
+    long double d;
+
+    printf("The size of short = %d bytes\n", sizeof(a));
+    printf("The size of long = %d bytes\n", sizeof(b));
+    printf("The size of long long = %d bytes\n", sizeof(c));
+    printf("The size of long double = %d bytes\n", sizeof(d));
+
+    return 0;
+}
